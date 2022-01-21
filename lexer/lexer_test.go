ef798381b59e6fcfeecc2d386fbb019048b31493
Giorgi Khunashvili <giorgi.khunashvili.1@btu.edu.ge>
2022-01-22 03:13:39 +0400

    added skipping whitespaces in lexer

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -61,8 +61,8 @@ let result = add(five, ten);
 	for i, tt := range tests {
 		tok := l.NextToken()
 		if tok.Type != tt.expectedType {
-			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q",
-				i, tt.expectedType, tok.Type)
+			t.Fatalf("tests[%d] - tokentype wrong. expected=%q, got=%q expected literal=%q got=%q",
+				i, tt.expectedType, tok.Type, tt.expectedLiteral, tok.Literal)
 		}
 		if tok.Literal != tt.expectedLiteral {
 			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q",
